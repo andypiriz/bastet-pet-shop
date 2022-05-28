1e0be903a2be8c92a6724442ee6d8478658ee19a
AndreaPiriz <68929750+AndreaPiriz@users.noreply.github.com>
2022-05-27 21:40:00 -0300

    Creé Navbar de la página

---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,18 @@
+import '../NavBar/NavBar.css'
+const Navbar = () => {
+    return (
+        <nav>
+            <ul>
+                <li><a href="">Gatos</a></li>
+                <li><a href="">Perros</a></li>
+                <li><a href="">Peces</a></li>
+                <li><a href="">Aves</a></li>
+                <li><a href="">Reptiles</a></li>
+                <li><a href="">Pequeñas mascotas</a></li>
+                <li><a href="">Contacto</a></li>
+            </ul>
+        </nav>
+    )
+}
+
+export default Navbar
